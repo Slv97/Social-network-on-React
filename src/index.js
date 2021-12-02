c8eff83739881e64c30c8f88e62a7c79752dc322
e.savoshinskaya <e.savoshinskaya@itransition.com>
2021-12-02 17:44:23 +0300

    Adding store

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import {addMessage, addPost, updateNewMessageText, updateNewPostText} from "./redux/state";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
-import state from './redux/state'
-import {subscribe} from './redux/state'
+import "./index.css";
+import App from "./App";
+import store from "./redux/store";
 
 let rerenderEntireTree = (state) => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} addMessage={addMessage} updateNewMessageText={updateNewMessageText} />
-      </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App
+        state={state}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
+        addMessage={store.addMessage.bind(store)}
+        updateNewMessageText={store.updateNewMessageText.bind(store)}
+      />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 }
 
-rerenderEntireTree(state)
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 reportWebVitals();
