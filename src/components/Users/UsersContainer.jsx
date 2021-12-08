2af1b10cf0511983f927d415009d4f2ef60bb2d3
e.savoshinskaya <e.savoshinskaya@itransition.com>
2021-12-08 20:32:41 +0300

    User component doing presentational component

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,51 @@
 import { connect } from "react-redux";
 import { followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC } from "../../redux/users-reducer";
+
+import * as axios from "axios";
+// import userPhoto from "../../assets/images/smile.svg";
+import React from "react";
 import Users from "./Users";
+
+class UsersAPIComponent extends React.Component {
+    componentDidMount() {
+      if (this.props.usersData.length === 0) {
+        axios
+          .get(
+            `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+          )
+          .then((response) => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+          });
+      }
+    }
+  
+    onPageChanged = (el) => {
+      this.props.setCurrentPage(el);
+      axios
+        .get(
+          `https://social-network.samuraijs.com/api/1.0/users?page=${el}&count=${this.props.pageSize}`
+        )
+        .then((response) => {
+          this.props.setUsers(response.data.items);
+        }); //el это  pageNumber
+    };
+  
+    render() {
+      return (
+        <Users
+          totalUsersCount={this.props.totalUsersCount}
+          pageSize={this.props.pageSize}
+          currentPage={this.props.currentPage}
+          onPageChanged={this.onPageChanged}
+          usersData={this.props.usersData}
+          unfollow={this.props.unfollow}
+          follow={this.props.follow}
+        />
+      );
+    }
+  }
+  
 
 let mapStateToProps = (state) => {
     return {
@@ -31,4 +76,4 @@ let mapDispatchToProps =(dispatch) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users); 
+export default connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent); 
