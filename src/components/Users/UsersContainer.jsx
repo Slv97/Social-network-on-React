ce4ad5a3b2fae9edf16a2133c1dd58e18ebb1bbe
e.savoshinskaya <e.savoshinskaya@itransition.com>
2021-12-09 15:23:22 +0300

    Added preloader and reduced mapDispatchToProps to object in method connect

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,38 +1,52 @@
 import { connect } from "react-redux";
-import { followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC } from "../../redux/users-reducer";
+import {
+  follow,
+  setCurrentPage,
+  setTotalUsersCount,
+  setUsers,
+  toggleIsFetching,
+  unfollow,
+} from "../../redux/users-reducer";
 
 import * as axios from "axios";
 // import userPhoto from "../../assets/images/smile.svg";
 import React from "react";
 import Users from "./Users";
+import Preloader from "../common/Preloader/Preloader";
 
 class UsersAPIComponent extends React.Component {
-    componentDidMount() {
-      if (this.props.usersData.length === 0) {
-        axios
-          .get(
-            `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
-          )
-          .then((response) => {
-            this.props.setUsers(response.data.items);
-            this.props.setTotalUsersCount(response.data.totalCount);
-          });
-      }
-    }
-  
-    onPageChanged = (el) => {
-      this.props.setCurrentPage(el);
+  componentDidMount() {
+    this.props.toggleIsFetching(true);
+    if (this.props.usersData.length === 0) {
       axios
         .get(
-          `https://social-network.samuraijs.com/api/1.0/users?page=${el}&count=${this.props.pageSize}`
+          `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
         )
         .then((response) => {
+          this.props.toggleIsFetching(false);
           this.props.setUsers(response.data.items);
-        }); //el это  pageNumber
-    };
-  
-    render() {
-      return (
+          this.props.setTotalUsersCount(response.data.totalCount);
+        });
+    }
+  }
+
+  onPageChanged = (el) => {
+    this.props.toggleIsFetching(true);
+    this.props.setCurrentPage(el);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${el}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.toggleIsFetching(false);
+        this.props.setUsers(response.data.items);
+      }); //el это  pageNumber
+  };
+
+  render() {
+    return (
+      <div>
+        {this.props.isFetching ? <Preloader /> : null}
         <Users
           totalUsersCount={this.props.totalUsersCount}
           pageSize={this.props.pageSize}
@@ -42,38 +56,49 @@ class UsersAPIComponent extends React.Component {
           unfollow={this.props.unfollow}
           follow={this.props.follow}
         />
-      );
-    }
+      </div>
+    );
   }
-  
+}
 
 let mapStateToProps = (state) => {
-    return {
-        usersData: state.usersPage.usersData,
-        pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage,
-    }
-}
+  return {
+    usersData: state.usersPage.usersData,
+    pageSize: state.usersPage.pageSize,
+    totalUsersCount: state.usersPage.totalUsersCount,
+    currentPage: state.usersPage.currentPage,
+    isFetching: state.usersPage.isFetching,
+  };
+};
 
-let mapDispatchToProps =(dispatch) => {
-    return {
-        follow: (userId) => {
-            dispatch(followAC(userId))
-        },
-        unfollow: (userId) => {
-            dispatch(unfollowAC(userId))
-        },
-        setUsers: (users) => {
-            dispatch(setUsersAC(users))
-        },
-        setCurrentPage: (currentPage) => {
-            dispatch(setCurrentPageAC(currentPage))
-        },
-        setTotalUsersCount: (totalUsersCount) => {
-            dispatch(setTotalUsersCountAC(totalUsersCount))
-        }
-    }
-}
+// let mapDispatchToProps = (dispatch) => {
+//   return {
+//     follow: (userId) => {
+//       dispatch(followAC(userId));
+//     },
+//     unfollow: (userId) => {
+//       dispatch(unfollowAC(userId));
+//     },
+//     setUsers: (users) => {
+//       dispatch(setUsersAC(users));
+//     },
+//     setCurrentPage: (currentPage) => {
+//       dispatch(setCurrentPageAC(currentPage));
+//     },
+//     setTotalUsersCount: (totalUsersCount) => {
+//       dispatch(setTotalUsersCountAC(totalUsersCount));
+//     },
+//     toggleIsFetching: (isFetching) => {
+//       dispatch(toggleIsFetchingAC(isFetching));
+//     },
+//   };
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent); 
+export default connect(mapStateToProps, {
+  follow,
+  unfollow,
+  setUsers,
+  setCurrentPage,
+  setTotalUsersCount,
+  toggleIsFetching,
+})(UsersAPIComponent);
---
+++ src/components/common/Preloader/Preloader.js
@@ -0,0 +1,9 @@
+import s from "./Preloader.module.css";
+
+const Preloader = () => {
+    return (
+         <div className={s.lds_dual_ring}> </div>
+    )
+}
+
+export default Preloader
